8344f31939f0e355a1454fe9fc85265fa1bed176
broadtube <brodtube@broadtube.xyz>
2021-03-06 19:21:40 +0900

    ES module update

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs';
 import {terser} from 'rollup-plugin-terser';
 
-export default {
+export default [{
   input: 'dist/socket.io.dev.js',
+  output: {
+    file: 'dist/socket.io.es.tmp.js',
+    format: 'es',
+  },
+  plugins: [commonjs()]
+},{
+  input: 'wrapper.mjs',
   output: {
     file: 'dist/socket.io.es.min.js',
     format: 'es',
   },
-  plugins: [commonjs(), terser()]
-};
+  plugins: [terser()]
+}];
---
+++ wrapper.mjs
@@ -0,0 +1,4 @@
+import socketIoClient from "./dist/socket.io.es.tmp.js";
+
+export const io = socketIoClient.io;
+export default io;
